0bc02fe6b5a10090d9f74629948103e3daac4d90
Danilogb18 <78026431+Danilogb18@users.noreply.github.com>
2025-07-30 13:26:06 -0500

    Add files via upload

---
+++ Taller10-JUnit/src/test/java/com/mycompany/taller10/junit/CalculatorSMPIT.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.taller10.junit;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author danilo
+ */
+public class CalculatorSMPIT {
+    
+    public CalculatorSMPIT() {
+    }
+
+    /**
+     * Test of main method, of class CalculatorSMP.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        CalculatorSMP.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ Taller10-JUnit/src/test/java/com/mycompany/taller10/junit/OperationsIT.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.taller10.junit;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author danilo
+ */
+public class OperationsIT {
+    
+    public OperationsIT() {
+    }
+
+    /**
+     * Test of MakeFormula method, of class Operations.
+     */
+    @Test
+    public void testMakeFormula() {
+        System.out.println("MakeFormula");
+        String expResult = "";
+        String result = Operations.MakeFormula();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of Solve method, of class Operations.
+     */
+    @Test
+    public void testSolve() {
+        System.out.println("Solve");
+        String formula = "";
+        String expResult = "";
+        String result = Operations.Solve(formula);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
